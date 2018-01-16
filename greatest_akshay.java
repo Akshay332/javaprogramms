908529f69c46b2eda2773afafe3559cfeaf699ff
Akshay332 <munnapathania475@gmail.com>
2018-01-16 14:35:09 +0530

    greatest

---
+++ greatest_akshay.java
@@ -0,0 +1,22 @@
+package class2;
+
+public class greatest_akshay {
+public static void main(String[]args){
+int i=30,a=60,b=40,c=25;
+if(a<i){
+	System.out.println("a is smaller");
+	System.out.println(a);
+}
+	else if(b>=i)
+	
+	{
+		System.out.println("b is greater");
+		System.out.println(b);
+		
+	}
+	else if(c>i){
+		System.out.println("c is greater");
+		System.out.println(c);
+}
+}
+}
