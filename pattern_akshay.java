31a6062e174e1db0b5932079b7adce96b48cb882
Akshay332 <munnapathania475@gmail.com>
2018-01-17 18:27:44 +0530

    pattern

---
+++ pattern_akshay.java
@@ -0,0 +1,17 @@
+package class2;
+
+public class pattern_akshay {
+	public static void main(String[]args){
+		int i=1;
+		while(i<=4){
+		int j=1;
+		while(j<=i){
+		System.out.print("*");	
+		j++;
+		}
+		i++;
+		System.out.println();
+	}
+
+}
+}
