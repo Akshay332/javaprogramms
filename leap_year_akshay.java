116af33add814a17e0334df34702406ffe5fc367
Akshay332 <munnapathania475@gmail.com>
2018-01-18 11:03:57 +0530

    leap

---
+++ leap_year_akshay.java
@@ -0,0 +1,17 @@
+package class2;
+
+public class leap_year_akshay {
+	public static void main(String[] args)
+	{
+		int year=2018;
+		if(year%4==0)
+		{
+			System.out.println("2018 is leap year");
+		}
+		else
+		{
+			System.out.println("2018 is not leap year");
+		}
+	}
+	}
+
