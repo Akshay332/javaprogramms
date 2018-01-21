76662fcd3e2ca10d544358dd407b6be1a5943ab9
Akshay332 <munnapathania475@gmail.com>
2018-01-21 11:15:26 +0530

    Christmas tree

---
+++ christmas.java
@@ -0,0 +1,17 @@
+package class2;
+
+public class christmas {
+public static void main(String[]args){
+	for(int i=0;i<4;i++){
+		for(int j=0;j<4-i;j++){
+			
+			System.out.print(" ");
+	}
+	for(int k=0;k<(2*i)+1;k++){
+	System.out.print("*");	
+	}
+		System.out.println();
+	
+}
+}
+}
