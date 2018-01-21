2db63ff0539fd9cd58035d16dfc92f64b7384434
Akshay332 <munnapathania475@gmail.com>
2018-01-21 11:10:07 +0530

    Palindrom

---
+++ Palindrom_akshay.java
@@ -0,0 +1,16 @@
+package class2;
+
+public class Palindrom_akshay {
+
+	public static void main(String[] args) {
+	int a=1,b=0,c;
+	c=a+b;
+	System.out.println(c);
+	for(int i=0;i<2;i++)
+	{
+		System.out.println(i);
+	}
+
+	}
+
+}
