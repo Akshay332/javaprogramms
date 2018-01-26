9dd0746da6b60110792e9fab44d6b2b46f7badbb
Akshay332 <munnapathania475@gmail.com>
2018-01-26 13:11:00 +0530

    2ndpattern

---
+++ Pattern2.java
@@ -0,0 +1,20 @@
+package class2;
+
+public class Pattern2 {
+public static void main(String[]args){
+	int i=1;
+	
+	while(i<6){
+		int j=5;
+		while(j>=i)
+		{
+			System.out.print("*");
+		j--;
+		
+		}
+		System.out.println();
+		i++;
+		
+	}
+}
+}
